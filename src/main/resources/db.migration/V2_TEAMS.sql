a9fb1bfbed9ea4211a4ffb1019d48d502a39fa5b
vicnetto <victorhmnetto@gmail.com>
2021-01-06 01:05:49 -0300

    :recycle: Refazendo o arquivo V2_TEAMS.sql e inserindo V5
    O V2_TEAMS.sql foi retornado para o seu formato original, e para isso
    foi adicionado o V5_TEAMS_ALTER.sql, promovendo uma alteração na tabela
    feita no V2. Antes essa modificação tinha sido realizado de forma
    errada, sendo inserida alterações nas versões ao invés de realizar o
    versionamento correto.

--- src/main/resources/db.migration/V2_TEAMS.sql
+++ src/main/resources/db.migration/V2_TEAMS.sql
@@ -1,10 +1,5 @@
 CREATE TABLE team(
-    team_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    acron VARCHAR(10),
-    wins INTEGER,
-    losses INTEGER,
-    goals_maden INTEGER,
-    goals_taken INTEGER,
-    goals_balance INTEGER
+     team_id SERIAL PRIMARY KEY,
+     name VARCHAR(255) NOT NULL,
+     acron VARCHAR(10)
 );
